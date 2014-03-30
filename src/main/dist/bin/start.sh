2c914fc8210ba1bd85d7c503302d92f41e5d774e
Kenneth Endfinger <kaendfinger@gmail.com>
2014-03-30 13:13:28 -0400

    Bug Fix in start.sh: Create the Launch Directory if it doesn't exist.

--- src/main/dist/bin/start.sh
+++ src/main/dist/bin/start.sh
@@ -3,6 +3,7 @@ DIST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd)"
 LAUNCH_DIR=""
 
 function launch() {
+    [ ! -d ${LAUNCH_DIR} ] && mkdir -p ${LAUNCH_DIR}
     cd ${LAUNCH_DIR}
     java -jar ${DIST_DIR}/lib/SimpleCI.jar
 }
