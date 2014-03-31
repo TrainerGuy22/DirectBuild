60a70df2ba8577bbfa1fdcdaea2cad5a061b08ff
Kenneth Endfinger <kaendfinger@gmail.com>
2014-03-31 10:46:14 -0400

    @NotNull for Context

--- src/main/groovy/org/directcode/ci/core/Context.groovy
+++ src/main/groovy/org/directcode/ci/core/Context.groovy
@@ -1,12 +1,13 @@
 package org.directcode.ci.core
 
 import groovy.transform.CompileStatic
+import org.jetbrains.annotations.NotNull
 
 @CompileStatic
 class Context {
     final CI ci
 
-    Context(CI ci) {
+    Context(@NotNull CI ci) {
         this.ci = ci
     }
 }
